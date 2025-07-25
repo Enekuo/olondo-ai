4991058778e8c04e251317d1eab72989c286b7a8
Eneko Aramberri <aramberrieneko@gmail.com>
2025-07-25 16:32:55 +0200

    Fondo 1

--- src/components/home/OlondoCTASection.jsx
+++ src/components/home/OlondoCTASection.jsx
@@ -8,7 +8,10 @@ const OlondoCTASection = () => {
   const { t } = useLanguage();
 
   return (
-    <section className="w-full bg-[#eef4ff] dark:bg-sky-900 py-40 px-6">
+    <section
+      className="w-full bg-no-repeat bg-cover bg-center pt-0 pb-40 px-6"
+      style={{ backgroundImage: "url('/images/cta-background.png')" }}
+    >
       <div className="container mx-auto max-w-4xl flex flex-col items-center text-center gap-8">
         <div className="bg-purple-100 dark:bg-purple-800 p-6 rounded-full">
           <Music className="text-purple-600 w-20 h-20" />
