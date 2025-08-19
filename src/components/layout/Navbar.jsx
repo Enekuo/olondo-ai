ee221b82a851bdb491534317a98e84619d528cd7
Eneko Aramberri <aramberrieneko@gmail.com>
2025-08-19 12:21:07 +0200

    Header25

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
               <img
                 src="/logo-olondo.png"
                 alt="Olondo AI Logo"
-                className="h-[40px] md:h-[60px] w-auto shrink-0"
+                className="h-[60px] md:h-[80px] w-auto shrink-0"
               />
             </Link>
 
