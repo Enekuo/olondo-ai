24d9fb5369708aef7129eb22ea02d2f57ddd5d3c
Eneko Aramberri <aramberrieneko@gmail.com>
2025-09-03 18:54:26 +0200

    Inform16

--- src/pages/CreateSummaryPage.jsx
+++ src/pages/CreateSummaryPage.jsx
@@ -25,6 +25,14 @@ const CreateSummaryPage = () => {
           {/* Mascota con bocadillo (texto desde traducciones) */}
           <div className="relative flex justify-center md:justify-start order-first">
             <div className="relative">
+              {/* Bocadillo ENCIMA de la mascota con triángulo hacia abajo */}
+              {speechText && speechText !== "create_summary_speech" && (
+                <div className="absolute -top-12 left-1/2 -translate-x-1/2 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-700 shadow-md px-4 py-2 rounded-2xl text-sm text-slate-800 dark:text-slate-200 relative">
+                  {speechText}
+                  <div className="absolute left-1/2 top-full -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-b-8 border-l-transparent border-r-transparent border-b-white dark:border-b-slate-900"></div>
+                </div>
+              )}
+
               <img
                 src="olondo-mascota2.png" // ajusta ruta/nombre si es distinto
                 alt={t("create_summary_image_alt")}
@@ -32,14 +40,6 @@ const CreateSummaryPage = () => {
                 draggable="false"
                 loading="eager"
               />
-              {/* Bocadillo con triángulo */}
-              {speechText && speechText !== "create_summary_speech" && (
-                <div className="absolute -top-6 left-1/2 -translate-x-1/2 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-700 shadow-md px-4 py-2 rounded-2xl text-sm text-slate-800 dark:text-slate-200 relative">
-                  {speechText}
-                  {/* Triángulo */}
-                  <div className="absolute left-1/2 -bottom-2 -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-white dark:border-t-slate-900"></div>
-                </div>
-              )}
             </div>
           </div>
 
